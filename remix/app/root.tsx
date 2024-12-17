91f2b83bdabdd4f07cffb8201884f044ab93bccf
Colin Tracy <colin.tracy@reline.solutions>
2024-12-16 21:23:31 -0600

    FEAT: add shadcn to remix

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -5,11 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css?url";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
-
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
